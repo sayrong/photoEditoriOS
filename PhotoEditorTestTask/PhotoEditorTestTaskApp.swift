185d22d6dee67057315100f03be1669d72b3b381
DmitrySK <dim127@mail.ru>
2025-04-18 17:19:05 +0300

    Initial Commit

---
+++ PhotoEditorTestTask/PhotoEditorTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorTestTaskApp.swift
+//  PhotoEditorTestTask
+//
+//  Created by DmitrySK on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
